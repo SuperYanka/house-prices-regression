38e39b1605ce44d609cbfe2503d56dc807842027
SuperYanka <superyanka13@gmail.com>
2025-08-01 17:08:52 +0300

    add EDA_kaggle and small fixes

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 
